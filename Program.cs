355fed120629f3750e6b4d070ee1ddc0247360a0
MaximeMarck30 <56680402+MaximeMarck30@users.noreply.github.com>
2019-11-06 19:40:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldMarckMaxime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world personnalisé de Marck Maxime et francois Sanglier");
+            Console.ReadLine();// present pour maintenair l'affichage vu que l'application se termine
+        }
+    }
+}
